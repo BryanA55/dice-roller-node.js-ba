b7eea47b0b5d6644605c993a71a805d7a35a6aa9
BryanA55 <bryanhavalos@lewisu.edu>
2024-10-07 00:04:34 -0500

    still struggle

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//const express = require('express');
-//const app = express();
+const express = require('express');
+const app = express();
 //const cors = require("cors");
 //const allowed = ['https://mango-mud-0f2f49210.5.azurestaticapps.net'];
 
